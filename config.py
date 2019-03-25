966dfc72686bf4b97d6687e6b58a2c2a5fb916e4
Digital Alchemist <38991296+anwaaralikhan@users.noreply.github.com>
2019-03-25 09:18:11 -0400

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ headers = {
   "Authorization":  "token" + TOKEN
 }
 GITHUB_API_URL = "https://api.github.com"
-orgs = ["/twitter/repos","/auth0/repos","/nasa/repos","/mozilla/repos","/adobe/repos/"]
+orgs = ["/twitter/repos","/auth0/repos","/nasa/repos","/mozilla/repos","/adobe/repos"]
